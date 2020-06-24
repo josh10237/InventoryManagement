d1d431aa302a91588445833ad9bf879fa17a39da
Josh Benson <josh.benson.805@gmail.com>
2020-06-24 16:15:04 -0700

    Added Splash

---
+++ InventoryManagement/app/src/main/java/com/example/inventorymanagement/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.example.inventorymanagement
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        startActivity(Intent(applicationContext, MainActivity::class.java))
+        finish()
+    }
+}
