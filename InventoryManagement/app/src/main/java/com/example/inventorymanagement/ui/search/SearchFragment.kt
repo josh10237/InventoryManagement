20e08bf9019ca8891b30802015cf90f5a98d533b
Josh Benson <josh.benson.805@gmail.com>
2020-06-26 21:02:09 -0700

    Search bar works like a fucking charm

--- InventoryManagement/app/src/main/java/com/example/inventorymanagement/ui/search/SearchFragment.kt
+++ InventoryManagement/app/src/main/java/com/example/inventorymanagement/ui/search/SearchFragment.kt
@@ -1,18 +1,25 @@
 package com.example.inventorymanagement.ui.search
 
 import android.os.Bundle
+import android.service.autofill.Validators.not
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.view.iterator
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.inventorymanagement.R
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.fragment_search.view.*
+import kotlinx.android.synthetic.main.fragment_search.*
 
 
 class SearchFragment : Fragment() {
@@ -28,15 +35,43 @@ class SearchFragment : Fragment() {
             ViewModelProviders.of(this).get(SearchViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_search, container, false)
         val searchProductBtn = root.findViewById<ImageButton>(R.id.searchBtn)
+        val search = root.findViewById(R.id.search_bar) as TextView
         val ll_main = root.findViewById(R.id.lin_layout) as LinearLayout
+        queryDatabase(ll_main)
 
-        searchProductBtn.setOnClickListener {
-            println("start")
-            queryDatabase(ll_main)
-            println("finish")
+//        search.setOnEditorActionListener { v, actionId, event ->
+//            if(actionId == EditorInfo.IME_ACTION_DONE){
+//                updateList(ll_main)
+//                true
+//            } else {
+//                false
+//            }
+//        }
+        search.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                updateList(ll_main)
+            }
 
-        }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+
         return root
+    }
+
+    fun updateList(linlay: LinearLayout){
+        var current_set = search_bar.text.toString()
+        for (btn in linlay) {
+            var b = btn as Button
+            val buttonText = b.text.toString()
+            if (buttonText.contains(current_set, ignoreCase = true)){
+                btn.setVisibility(View.VISIBLE)
+            }
+            else {
+                btn.setVisibility(View.GONE)
+            }
+        }
     }
 
     fun queryDatabase(linlay: LinearLayout) {
@@ -56,9 +91,7 @@ class SearchFragment : Fragment() {
         println(name)
         println(t_id)
         println(quantity)
-        // creating the button
         val button_dynamic = Button(activity)
-        // setting layout_width and layout_height using layout parameters
         button_dynamic.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         button_dynamic.text = name + "   " + t_id
         // add Button to LinearLayout
